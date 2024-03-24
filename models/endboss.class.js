c7b3d04f3ed889ffcf9fd85667ccdd215ef3f597
Sascha Schulze <sascha-schulze1@web.de>
2024-03-24 19:52:07 +0100

    walking boss function added

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -3,9 +3,15 @@ class Endboss extends moveableObject {
     height = 400;
     width = 300;
     y = 70;
-    
 
     IMAGES_WALKING = [
+        'img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/1_walk/G4.png',
+    ];
+
+    IMAGES_ANGRY = [
         'img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G5.png',
         'img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G6.png',
         'img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G7.png',
@@ -16,16 +22,62 @@ class Endboss extends moveableObject {
         'img_pollo_locco/img/4_enemie_boss_chicken/2_alert/G12.png'
     ];
 
+    IMAGES_ATTACK = [
+        'img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/3_attack/G20.png',
+    ];
+
+    IMAGES_HURT = [
+        'img_pollo_locco/img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/4_hurt/G23.png',
+    ];
+
+    IMAGES_DEAD = [
+        'img_pollo_locco/img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img_pollo_locco/img/4_enemie_boss_chicken/5_dead/G26.png',
+    ];
+
     constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
+        super().loadImage(this.IMAGES_ANGRY[0]);
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_ANGRY);
+        this.loadImages(this.IMAGES_ATTACK);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 2500;
-        this.animate();
+        this.animateAngry();
+        this.isDead = false;
+        this.animateWalkingBoss();
     }
 
-    animate() {
+    animateAngry() {
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+            this.playAnimation(this.IMAGES_ANGRY);
         }, 200)
+    }
+
+    animateWalkingBoss() {
+        let walkingIndex = 0;
+    
+        setInterval(() => {
+            if (!this.isDead) {
+                this.moveLeft();
+            }
+        }, 1000 / 60); // Bewegungsintervall: 60x pro Sekunde wird bewegt
+    
+        setInterval(() => {
+            if (!this.isDead) {
+                this.loadImage(this.IMAGES_WALKING[walkingIndex]);
+                walkingIndex = (walkingIndex + 1) % this.IMAGES_WALKING.length;
+            }
+        }, 200);
     }
 }
